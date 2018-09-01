8ceb4e0c05dc6526bf5c994af65714890e65bd86
rohithv <rohith.vishnu333@gmail.com>
2018-09-01 14:58:22 +0530

    ReadPage and FlushPage

--- physical/flushpage.c
+++ physical/flushpage.c
@@ -1,6 +1,33 @@
 
+/*
+ * @author: rohith
+ */
 
-FlushPage()
+int FlushPage(int relNum)
 {
- printf("FlushPage \n ");
+	//flushes the current page 
+	char page[PAGESIZE]= {0};
+
+	//check for errors
+	if(relNum<0 || relNum>=MAXOPEN){
+		//display error
+		return NOTOK;
+	}
+
+	if(buffer[relNum].modified==false){
+		return OK;
+	}
+
+	unsigned int slotmap = buffer[relNum].page.slotmap;
+
+	CopyIntInBinary(page, slotmap);
+	strncpy(page+SLOTMAPSIZE, buffer[relNum].page.contents, MAXRECORD);
+
+	int offset = buffer[relNum].pid * PAGESIZE;
+	write(catcache[relNum].relFile, page+offset, PAGESIZE);
+
+	buffer[relNum].modified=false;
+
+	return OK;
+
 }
--- physical/readpage.c
+++ physical/readpage.c
@@ -1,6 +1,56 @@
 
+/*
+ * @author: rohith
+ */
 
-ReadPage()
+int ReadPage(int relNum, short pid)
 {
- printf("ReadPage \n ");
+	char page[PAGESIZE] = {0};
+	int retVal, offset;
+
+	if(relNum<0 || relNum>=MAXOPEN){
+		//print error
+		return NOTOK;
+	}
+
+	if(pid > catcache[relNum].numPgs || pid<0){
+		//print error
+		return NOTOK;
+	}
+	else if(pid == buffer[relNum].pid){ //Already present in buffer.
+		return OK;
+	}
+	else if(pid == catcache[relNum].numPgs){ //A new page is being requested.
+		if(buffer[relNum].modified == true){
+			retVal = FlushPage(relNum);
+			if(retVal == NOTOK){
+				return NOTOK;
+			}
+		}
+
+		buffer[relNum].pid = pid;
+		buffer[relNum].page.slotmap = 0;
+		buffer[relNum].modified= false;
+	}
+	else if(pid < catcache[relNum].numPgs){ //Existing page is to be read.
+
+		if(buffer[relNum].modified == true){
+			retVal = FlushPage(relNum);
+			if(retVal == NOTOK){
+				return NOTOK;
+			}
+		}
+
+		offset = pid * PAGESIZE;
+
+		read(catcache[relNum].relFile, page, PAGESIZE);
+
+		buffer[relNum].page.slotmap = ReadIntFromBinary(page);
+		strncpy(buffer[relNum].page.contents, page, MAXRECORD);
+
+		buffer[relNum].pid = pid;
+		buffer[relNum].modified= false;
+	}
+
+	return OK;
 }
