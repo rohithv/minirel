49e84861831dff8c73cbe264e035f5e7d84436f4
priyatamsai <priyatamsai7@gmail.com>
2018-09-03 23:19:35 +0530

    ^X ^x

--- physical/deleterec.c
+++ physical/deleterec.c
@@ -1,6 +1,25 @@
+/*
+ * @author: Priyatam
+ */
 
-
-DeleteRec()
+int DeleteRec(int relnum, Rid *rid)
 {
- printf("DeleteRec \n ");
+
+
+	if(readpage(relnum,rid->pid)==NOTOK) return NOTOK;
+	
+	short slot=rid->slotnum;
+	
+	if( (1<<((SLOTMAPSIZE * 8) - slot -1)) & buffer[relnum].page.slotmap ){
+		
+		buffer[relnum].modified=TRUE;
+		buffer[relnum].page.slotnum=(buffer[relnum].page.slotnum ^ (1<<((SLOTMAPSIZE * 8) - slot -1)) );
+	}
+	
+	return OK;
+	
+
 }
+
+
+
--- physical/writerec.c
+++ physical/writerec.c
@@ -1,6 +1,18 @@
+/*
+ * @author: priyatam
+ */
 
-
-WriteRec()
+int WriteRec(int relnum, char *recPtr,RID *recRid)
 {
- printf("WriteRec \n ");
+	if(readpage(relnum,rid->pid)==NOTOK) return NOTOK;
+	
+	short slot=rid->slotnum;
+	
+	if(slot > catcache[relnum].recsPerPg ) return NOTOK;
+	
+	
+	buffer[relnum].page.slotnum=(buffer[relnum].page.slotnum | (1<<((SLOTMAPSIZE * 8) - slot -1)) );
+	buffer[relnum].modified=TRUE;
+	strncpy(buffer[relnum].page.contents + SLOTMAPSIZE*8 + (slot *RECORDSIZE) , recPtr, RECORDSIZE);
+
 }
